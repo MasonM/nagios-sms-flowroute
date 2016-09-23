da4d4e9987b0b3e0052b374d809e15be30b577c0
Mason Malone <mason@masonm.org>
2016-09-23 17:33:13 -0400

    Fix project name in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='nagios-flowroute-sms',
+setup(name='nagios-sms-flowroute',
     version='0.1',
     description='Nagios plugin to send SMS alerts via Flowroute',
     license='BSD',
     author='Mason Malone',
     author_email='mason.malone@gmail.com',
-    url='http://github.com/MasonM/nagios-flowroute-sms/',
+    url='http://github.com/MasonM/nagios-sms-flowroute/',
     scripts=['nagios_sms_flowroute.py'],
     zip_safe=False,
     classifiers=[
