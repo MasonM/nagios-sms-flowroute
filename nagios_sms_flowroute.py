28d0c3804fc5dddc379dc0af538680df6e1354a6
Mason Malone <mason@masonm.org>
2016-09-22 23:41:18 -0400

    Minor cleanup

--- nagios_sms_flowroute.py
+++ nagios_sms_flowroute.py
@@ -21,27 +21,33 @@ import pprint
 ACCESS_KEY = '1111111'
 SECRET_KEY = 'longbase64stringhere'
 
-def parse_cmdline_args():
+def parse_cmdline():
     usage = "usage: %prog [--debug] FROM_NUMBER TO_NUMBER MESSAGE"
-    description = "Send SMS message given by MESSAGE to TO_NUMBER, with " +\
-        "FROM_NUMBER as the sender. Example:\n" +\
+    description = (
+        "Send SMS message given by MESSAGE to TO_NUMBER, with "
+        "FROM_NUMBER as the sender. Example:\n"
         "%prog 11234567891 11234567890 \"foobar\""
+    )
     parser = optparse.OptionParser(usage=usage, description=description)
     parser.add_option('--debug', action='store_true',
                       help="Print debugging info to STDOUT")
     (options, args) = parser.parse_args()
     if len(args) != 3:
-        parser.error("incorrect number of arguments")
-    return options.debug, args
+        parser.error("Expected three arguments, FROM_NUMBER, TO_NUMBER, and MESSAGE")
+    return options.debug, {
+        "from_number": args[0],
+        "to_number": args[1],
+        "message": args[2],
+    }
 
 def send_message(to_number, from_number, message):
     message = Message(to=to_number, from_=from_number, content=message)
     controller = APIController(username=ACCESS_KEY, password=SECRET_KEY)
     return controller.create_message(message)
 
-debug, args = parse_cmdline_args()
+debug, params = parse_cmdline()
 try:
-    response = send_message(from_number=args[0], to_number=args[1], message=args[2])
+    response = send_message(**params)
     if debug:
         pprint.pprint(response)
 except APIException as e:
